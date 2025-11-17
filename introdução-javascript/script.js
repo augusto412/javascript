e1b42d6a26750d90ed91dbc39484276cd60770a5
augusto neto <augustoneto0213@gmail.com>
2025-11-17 20:12:38 -0300

    conseito de java

---
+++ introdução-javascript/script.js
@@ -0,0 +1,11 @@
+//alert("Olá,Mundo")//
+console.log(50) //number
+console.log("teste") //string
+console.log(true, false) //boolean (true ou false)
+console.log(null) //vazio
+
+let pessoa = {
+nome:"Augusto",
+idade:21,
+}
+console.log(pessoa)
