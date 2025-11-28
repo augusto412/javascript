1e05cba3be1d92c1a03e3ccf6c8c8bcd32972748
augusto neto <augustoneto0213@gmail.com>
2025-11-28 20:06:31 -0300

    projeto-de-nortas

--- objetos/PROJETO.js
+++ objetos/PROJETO.js
@@ -1,15 +1,39 @@
 
-
-const gerenciadorDeNotas = {
+const gerenciadorDeNotas ={
     notas: [
-        {titulo: "levar o lixo", conteudo: "a mulher já esta brava!"}
+        { id: 1, titulo: "jogar o lixo fora", conteudo: "minha mulher vai brigar"}
     ],
 
-    adicionarNota: function(titulonovo, conteudonovo){
-        this.notas.push ({titulo:titulonovo, conteudo: conteudonovo} )
+    adicionarNota: function( idnovanota, titulo, conteudo) {
+        this.notas.push({ idnovanota, titulo, conteudo})
+    },
+
+    buscarNota: function(titulo) {
+        return this.notas.find((nota) => nota.titulo === titulo);
+    },
+
+    FiltraNota: function(titulo){
+        return this.notas.filter ((nota) => nota.titulo.includes(titulo));
+    },
+
+    atualizarNota: function(id, tituloEditado, conteudoEditado){
+       const index = (this.notas.findIndex((nota) => nota.id === id));
+
+       if(!index){
+        return "não encontrado"
+       }
+       
+       gerenciadorDeNotas.notas[index] = {id: id, titulo:tituloEditado, conteudo:conteudoEditado};
+
+       return gerenciadorDeNotas.notas;
     }
 }
 
-gerenciadorDeNotas.adicionarNota("lavar os pratos", "mãe ja esta brigando");
+gerenciadorDeNotas.adicionarNota(2,"lavar roupa", "todas estão sujas");
+gerenciadorDeNotas.adicionarNota(3,"comprar novo teclado", "esse esta horrivbel");
+gerenciadorDeNotas.adicionarNota(4,"estudar 4 aulas por dia!", "modulo 2 de JS!");
+gerenciadorDeNotas.adicionarNota(5,"lembrar de tomar cafe antes das aulas", "voce sempre deixar esfirar");
 
-console.log(gerenciadorDeNotas.notas)
+gerenciadorDeNotas.atualizarNota(2, "nota editada", "nova nota")
+
+console.log(gerenciadorDeNotas.FiltraNota("aulas"))
